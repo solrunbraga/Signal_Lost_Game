39e8bad4eb9df635e6753aa4db3b1001e2187ee5
Sólrún <solrun.bragab@gmail.com>
2026-03-02 16:36:20 +0000

    footsteps

--- Assets/Scripts/F_CharFst.cs
+++ Assets/Scripts/F_CharFst.cs
@@ -4,101 +4,53 @@ using FMOD.Studio;
 
 public class F_CharFst : MonoBehaviour
 {
-    private int MaterialValue; 
-    private RaycastHit rh; 
-    private float distance = 0.3f; 
-    private string EventPath = "event:/char/footsteps";
-    private PARAMETER_ID ParamID; 
-    private PARAMETER_ID ParamID2; 
-    private LayerMask lm;
-    //Section Used for finding FMOD parameter ID number (instead of name)
-    /*
-    private EventDescription EventDes;
-    private PARAMETER_DESCRIPTION ParamDes;
-    */
+    private enum CURRENT_TERRAIN { CONCRETE };
+    [SerializeField] 
+    private CURRENT_TERRAIN currentTerrain;
 
-    private void Start()
+    private FMOD.Studio.EventInstance footsteps;
+
+    private void Update()
     {
-        // This section returns the parameter ID in the console
-        
-        /* EventDes = RuntimeManager.GetEventDescription(EventPath);
-        EventDes.getParameterDescriptionByName("Terrain", out ParamDes);
-        ParamID = ParamDes.id;
-        Debug.Log(ParamID.data1 + " " + ParamID.data2);*/
-        
-
-        // Assigns the ID of 'Terrain'
-        ParamID.data1 = 1082362436;
-        ParamID.data2 = 3768982689;
-        // Assigns the ID for 'WalkRun'
-        ParamID2.data1 = 4001576615;
-        ParamID2.data2 = 3268475985;
-        lm = LayerMask.GetMask("Ground");        
+        DetermineTerrain(); 
     }
 
-    /*
-    void Update()
+    private void DetermineTerrain()
     {
-        // Shows drawn raycast for debugging
-        Debug.DrawRay(transform.position, Vector3.down * distance, Color.blue);
-    }
-    */
+        RaycastHit[] hit;
 
-    void PlayRunEvent()
-    {
-        // Start with material check then instantiate sound
-        MaterialCheck();
-        EventInstance Run = RuntimeManager.CreateInstance(EventPath);
-        RuntimeManager.AttachInstanceToGameObject(Run, transform, GetComponent<Rigidbody>());
+        hit = Physics.RaycastAll(transform.position, Vector3.down, 10.0f);
 
-        // Sets the Terrain parameter
-        Run.setParameterByID(ParamID, MaterialValue, false);
-
-        // Sets the 'WalkRun' parameter
-        Run.setParameterByID(ParamID2, 1, false);
-
-        // Can be used as alternative to IDs
-        //Run.setParameterByName("Terrain", MaterialValue);
-        //Run.setParameterByName("WalkRun", 1, false);
-
-        Run.start();
-        Run.release();
-    }
-
-    void PlayWalkEvent()
-    {
-        // Start with material check then instantiate sound
-        MaterialCheck();
-        EventInstance Walk = RuntimeManager.CreateInstance(EventPath);
-        RuntimeManager.AttachInstanceToGameObject(Walk, transform, GetComponent<Rigidbody>());
-
-        // Sets the Terrain parameter
-        Walk.setParameterByID(ParamID, MaterialValue, false);
-
-        // Sets the 'WalkRun' parameter
-        Walk.setParameterByID(ParamID2, 1, false);
-
-        // Can be used as alternative to IDs
-        // Run.setParameterByName("Terrain", MaterialValue);
-
-        Walk.start();
-        Walk.release();
-    }
-
-    // Sets parameter based on RaycastHit
-    void MaterialCheck()
-    {
-
-        if (Physics.Raycast(transform.position, Vector3.down, out rh, distance, lm))
+        foreach (RaycastHit rayhit in hit)
         {
-            //Debug.Log(rh.collider.tag + " " + MaterialValue);
-            switch (rh.collider.tag)
+            if (rayhit.transform.gameObject.layer == LayerMask.NameToLayer("Concrete"))
             {
-                case "Concrete":
-                    MaterialValue = 0; // Labeled parameters in FMOD
-                    break;
-
+                currentTerrain = CURRENT_TERRAIN.CONCRETE;
+                break; 
             }
         }
+    }
+
+    public void SelectAndPlayFootstep()
+    {
+        switch (currentTerrain)
+        {
+            case CURRENT_TERRAIN.CONCRETE:
+                PlayFootstep(0); 
+                break;
+            
+            default:
+                PlayFootstep(0); 
+                break; 
+        }
+    }
+
+    private void PlayFootstep(int terrain)
+    {
+        footsteps = FMODUnity.RuntimeManager.CreateInstance("event:/Char/footsteps");
+        footsteps.setParameterByName("Terrain", terrain);
+        footsteps.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
+        footsteps.start();
+        footsteps.release();
     }
 }
